87cee54d220478104190f6632c5b6dfa4cfe28a0
Daniel Nabok <Artkvadrat984@gmail.com>
2019-11-04 22:33:57 +0200

    Added a filter option to ItemAddPanel. It check is too many symbols in input or no and is in input incorrect symbols.

--- src/components/ItemAddPanel/ItemAddPanel.js
+++ src/components/ItemAddPanel/ItemAddPanel.js
@@ -4,12 +4,35 @@ import './ItemAddPanel.css';
 class ItemAddPanel extends Component {
 
     state ={
-        label: ''
+        label: '',
+        valid: true,
+        checked: true
     };
 
     onLabelChange = (event) => {
-        this.setState({
-           label: event.target.value
+        let text = event.target.value;
+        this.setState( () => {
+            let invalidSymbols = "*|,\":<>[]{}`';()@&$#%"; // checking for invalid symbols
+            let checked = true;
+            for (let i = 0; i < text.length; i++) {
+                if (invalidSymbols.indexOf(text.charAt(i)) !== -1) {
+                    checked = false;
+                }
+            }
+            let valid = text.length <= 25; // checking for valid length of line
+
+            if ( valid && checked ) {
+                return{
+                    label: text,
+                    valid: valid
+                }
+            } else {
+                return {
+                    valid: valid,
+                    checked: checked
+                }
+            }
+
         });
     };
 
@@ -24,6 +47,19 @@ class ItemAddPanel extends Component {
     };
 
     render() {
+        const { valid, checked } = this.state;
+        if ( valid && checked ) {
+            this.tabooClass = 'none';
+            this.tabooText = '';
+        } else if ( !valid ) {
+            this.tabooText = 'Too many symbols. Please use less than 25 symbols.';
+            this.tabooClass = 'active color';
+        } else if (!checked ) {
+            this.tabooClass = 'active color';
+            this.tabooText = 'Incorrect symbols. Please don\'t use symbols like this: *|,\\":<>[]{}`\';()@&$#%"';
+        }
+
+
         return (
             <div>
                 <form className="d-flex justify-content-between addPanelForm"
@@ -35,8 +71,9 @@ class ItemAddPanel extends Component {
                            value={ this.state.label }/>
                     <button className="btn btn-outline-info" >Add Item</button>
                 </form>
+                <p className={ this.tabooClass }> { this.tabooText } </p>
             </div>
-        )
+        );
     }
 }
 
